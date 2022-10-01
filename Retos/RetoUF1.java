4a9f28359842f245bd535f765afb918d02215a4a
PCM28 <pierre.andres@bootcamp-upgrade.com>
2022-10-01 12:40:31 +0200

    Reto1 Updated

--- Retos/RetoUF1.java
+++ Retos/RetoUF1.java
@@ -4,15 +4,15 @@ public class RetoUF1 {
    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         boolean correct = false;
-        System.out.println("Introduce la hora: ");
+        System.out.print("Introduce la hora: ");
         int hora = entrada.nextInt();
-        System.out.println("Introduce el minuto: ");
+        System.out.print("Introduce el minuto: ");
         int minuto = entrada.nextInt();
-        System.out.println("Introduce el segundo: ");
+        System.out.print("Introduce el segundo: ");
         int segundo = entrada.nextInt();
         entrada.close();
         if((hora<24 && hora>=0) && (minuto<60 && minuto>=0) && (segundo<60 && segundo>=0)) correct = true; 
         if(correct) System.out.println("La hora es correcta");
-        else System.out.println("La hora NO es incorrecta");
+        else System.out.println("La hora NO es correcta");
     }
 }
